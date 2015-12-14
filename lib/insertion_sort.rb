48c07793e175826a2cfa2c430e42d73b3a123f92
Scott Firestone <scott.firestone.dev@gmail.com>
2015-12-13 20:09:00 -0700

    nil case fixed for insertion sort

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -3,23 +3,27 @@ require 'pry'
 class InsertionSort
 
   def sort(unsorted_array)
-    sorted_array = []
-    swap = true
-    unsorted_array.each do |element|
-      index = 0
-      swap = false
-      while swap == false
-        if sorted_array[index] == nil
-          sorted_array << element
-          swap = true
-        elsif element > sorted_array[index]
-          index +=1
-        else
-          sorted_array.insert(index, element)
-          swap = true
+    if unsorted_array == nil
+      puts "I'm sorry Dave, I'm afraid I can't do that."
+    else
+      sorted_array = []
+      swap = true
+      unsorted_array.each do |element|
+        index = 0
+        swap = false
+        while swap == false
+          if sorted_array[index] == nil
+            sorted_array << element
+            swap = true
+          elsif element > sorted_array[index]
+            index +=1
+          else
+            sorted_array.insert(index, element)
+            swap = true
+          end
         end
       end
+      sorted_array
     end
-    sorted_array
   end
 end
